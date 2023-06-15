bb1f37f9d1a4dff66b12f455d3e99edb41dd597f
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-15 14:10:24 +0500

    Still throw same error

--- src/main/java/com/thymeleaf/controller/MyController.java
+++ src/main/java/com/thymeleaf/controller/MyController.java
@@ -1,6 +1,6 @@
 package com.thymeleaf.controller;
 
-import com.thymeleaf.model.LogIn;
+import com.thymeleaf.model.LoginData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -52,16 +52,17 @@ public class MyController {
     }
 
 
-    @GetMapping("/login")
-    public String LogIn(Model model){
-        model.addAttribute("logIn", new LogIn());
-        return "Login";
+    @GetMapping("/form")
+    public String openForm(Model model){
+        System.out.println("opening form");
+        model.addAttribute("loginData", new LoginData());
+        return "form";
     }
 
 
     //handle for process form
     @PostMapping("/process")
-    public String processForm(@Valid @ModelAttribute("LogIn") LogIn logIn, BindingResult result){
+    public String processForm(@Valid @ModelAttribute("loginData") LoginData loginData, BindingResult result){
 
         if(result.hasErrors()){
             System.out.println(result);
--- src/main/java/com/thymeleaf/model/LoginData.java
+++ src/main/java/com/thymeleaf/model/LoginData.java
@@ -12,9 +12,25 @@ import javax.validation.constraints.Size;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-public class LogIn {
+public class LoginData {
     @NotBlank(message = "Username cannot be empty")
     @Size(min = 3,max = 12, message = "Username must be between 3 to 12 characters")
-    private String userName;
-    private String email;
+    public String userName;
+    public String email;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
